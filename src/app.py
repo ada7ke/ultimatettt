dce240138d84189c39052abde9205e77fd62fdc9
Ada <adayke10@gmail.com>
2023-05-12 08:19:49 -0700

    Co-authored-by: yanxke <yanxke@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -2,32 +2,43 @@ from loguru import logger
 import streamlit as st
 import ultimate
 
-def main():
-    st.set_page_config(page_title="Ultimate Tic Tac FEET", layout="wide", initial_sidebar_state="collapsed")
-    board = ultimate.Board()
-    playing = True
-    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    keyCounter = 0
+# streamlit run src/app.py
 
-    while playing == True:
-        st.write(f"Player {board.current_player} turn")
-        boardMarkdown = f"```\nUltimate Tic Tac FEET\n{board.printBoard()}\n```"
-        st.write(boardMarkdown)
-        playerInput = st.text_input("Please choose a square: ", key=f"move{keyCounter}")
-        keyCounter += 1  
-        if len(playerInput) > 0:
-            while playerInput not in numbers or board.isOccupied(int(playerInput)):
-                playerInput = st.text_input("Please choose a square: ", key=f"move{keyCounter}")
-                keyCounter += 1
-            playerInput = int(playerInput)
-            board.move(playerInput)
-            st.write("\n")
-            
-            score = board.scoring()
-            if score != ultimate.Marker.EMPTY:
-                st.write(f"Player {score} wins!")
-                return
-            
+def main():
+    st.set_page_config(page_title="Ultimate Tic Tac Toe", layout="wide", initial_sidebar_state="collapsed")
+    if "board" in st.session_state:
+        board = st.session_state["board"]
+    else:
+        board = ultimate.Board()
+        st.session_state["board"] = board
+
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+
+    st.write(f"Player {board.current_player} turn")
+    boardMarkdown = f"```\nUltimate Tic Tac Toe\n{board.printBoard()}\n```"
+    st.write(boardMarkdown)
+    playerInput = st.text_input("Please choose a square: ", key=f"move")
+    if len(playerInput) > 0:
+        if playerInput not in numbers or board.isOccupied(int(playerInput)):
+            st.write("Invalid input. Please try again.")
+            return
+        playerInput = int(playerInput)
+        board.move(playerInput)
+        st.session_state.board = board
+
+        st.write("\n")
+        
+
+        # score = board.scoring()
+        # print(f"Score: '{score}'")
+        # if score != ultimate.Marker.EMPTY and score != " ":
+        #     st.write(f"Player {score} wins!")
+        #     return
+        
+    st.experimental_rerun()
+        
+    st.write(ultimate.Board())
+              
 
 if __name__ == "__main__":
     main()
